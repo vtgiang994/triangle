994b3c6e839438d097ea4c1426aed9a1f5b89987
G7 <motsommaibinhyen94@gmail.com>
2020-09-20 16:27:06 +0700

    init project

---
+++ index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="css/style.css?v=2" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="js/jquery-3.5.1.min.js"></script>
+</head>
+
+<body>
+    <header id="header">
+        <!-- <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                <ul class="nav nav-pills">
+                    <li><a href=""><i class="fa fa-facebook" ></i></a></li>
+                    <li><a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    <li><a href=""><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                    <li><a href=""><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+                    <li><a href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                </ul>
+                </div>
+            </div>
+        </div> -->
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 overflow">
+                    <div class="social-icons pull-right">
+                        <ul class="nav nav-pills">
+                            <li><a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                            <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                            <li><a href=""><i class="fa fa-google-plus"></i></a></li>
+                            <li><a href=""><i class="fa fa-dribbble"></i></a></li>
+                            <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="index.html">
+                            <h1><img src="img/logo.png" alt="logo"></h1>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-8">
+                    <div class="menu">
+                        <nav class="navbar navbar-expand-lg navbar-right">
+                            <div class="collapse navbar-collapse">
+                                <ul class="nav navbar-nav navbar-right justify-content-end">
+                                    <li class="nav-item active">
+                                        <a class="nav-link" href="index.php">Home</a>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Page
+                                        </a>
+                                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="#">About</a>
+                                            <a class="dropdown-item" href="#">Another action</a>
+                                            <a class="dropdown-item" href="#">Something else here</a>
+                                            <a class="dropdown-item" href="#">Action</a>
+                                            <a class="dropdown-item" href="#">Action</a>
+                                            <a class="dropdown-item" href="#">Action</a>
+                                            <a class="dropdown-item" href="#">Action</a>
+                                            <a class="dropdown-item" href="#">Action</a>
+                                        </div>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Blog
+                                        </a>
+                                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="#">Action</a>
+                                            <a class="dropdown-item" href="#">Another action</a>
+                                            <a class="dropdown-item" href="#">Something else here</a>
+                                            <a class="dropdown-item" href="#">Another action</a>
+                                            <a class="dropdown-item" href="#">Another action</a>
+                                            <a class="dropdown-item" href="#">Another action</a>
+                                        </div>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Portfolio
+                                        </a>
+                                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="#">Action</a>
+                                            <a class="dropdown-item" href="#">Another action</a>
+                                            <a class="dropdown-item" href="#">Something else here</a>
+                                            <a class="dropdown-item" href="#">Action</a>
+                                            <a class="dropdown-item" href="#">Action</a>
+                                            <a class="dropdown-item" href="#">Action</a>
+                                        </div>
+                                    </li>
+                                    <li class="nav-item active">
+                                        <a class="nav-link" href="#">Shortcodes</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                        
+                        
+                    </div>
+                    <div class="search">
+                            <form>
+                            <i class="fa fa-search"></i>
+                            <div class="field-toggle">
+                            <input type="text" name="Search" value="" placeholder="Search">    
+                            </div>
+                            
+                            </form>
+                        
+                        </div>
+                </div>
+            </div>
+
+
+        </div>
+    </header>
+    <section id="home-slider">
+        <div class="container">
+            <div class="main-slider">
+                <div class="slide-text">
+                    <div>
+                        <h1>We Are Creative Nerds</h1>
+                        <p>Boudin doner frankfurter pig. </p>
+                        <a href="#" class="btn btn-sign">Sign up</a>
+                    </div>
+
+                </div>
+                <img src="img/home/slider/slide1/house.png" class="sld-house">
+                <img src="img/home/slider/slide1/circle1.png" class="sld-circle1">
+                <img src="img/home/slider/slide1/circle2.png" class="sld-circle2">
+                <img src="img/home/slider/slide1/cloud1.png" class="sld-cloud1">
+                <img src="img/home/slider/slide1/cloud2.png" class="sld-cloud2">
+                <img src="img/home/slider/slide1/cloud3.png" class="sld-cloud3">
+                <img src="img/home/slider/slide1/sun.png" class="sld-sun">
+                <img src="img/home/cycle.png" class="sld-cycle">
+            </div>
+    </section>
+
+    <section id="servicer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <img src="img/home/icon1.png" alt="">
+                    <h2>Incredibly Responsive</h2>
+                    <p>Ground round tenderloin flank shank ribeye. Hamkevin meatball swine. Cow shankle beef sirloin chicken ground round.</p>
+                </div>
+                <div class="col-sm-4">
+                    <img src="img/home/icon2.png" alt="">
+                    <h2>Superior Typography</h2>
+                    <p>Hamburger ribeye drumstick turkey, strip steak sausage ground round shank pastrami beef brisket pancetta venison.</p>
+                </div>
+                <div class="col-sm-4">
+                    <img src="img/home/icon3.png" alt="">
+                    <h2>Swift Page Builder</h2>
+                    <p>Venison tongue, salami corned beef ball tip meatloaf bacon. Fatback pork belly bresaola tenderloin bone pork kevin shankle.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    </div>
+
+    <div class="footer">
+
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
